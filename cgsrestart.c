6f8e8b19e7688d0d75b10ce13d3b91b327938e8f
Sebastien Nicoud <snicoud@expedia.com>
2019-01-22 12:41:01 -0500

    C code to add setuidable wrapper for cgs service restart

---
+++ cgsrestart.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[]) {
+	char cmd[99];
+	if (argc > 2) snprintf(cmd, 99, "/sbin/service %s %s", argv[1], argv[2]);
+	else if (argc > 1) snprintf(cmd, 99, "/sbin/service %s restart", argv[1]);
+	else snprintf(cmd, 99, "/sbin/service cgs-batch restart");
+	setuid(0);
+	system(cmd);
+	return 0;
+}
